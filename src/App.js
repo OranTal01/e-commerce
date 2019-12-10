08a3060b682f23bf4516b6d22d77e95e388e5541
oran <orantal01@gmail.com>
2019-12-10 21:13:07 +0200

    added home page menu-item and directory

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/home-page';
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
